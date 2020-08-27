171c3bf87d14d3958b6177c46beac24f0e679aea
Hakan625 <56585381+Hakan625@users.noreply.github.com>
2020-08-27 12:07:16 +0200

    Update pi_self_locating_strings.php
    Tidied up the parameters of function

--- pi_self_locating_strings.php
+++ pi_self_locating_strings.php
@@ -5,7 +5,12 @@ $pi = file_get_contents('pi_one_billion.txt');
 $pi_length = strlen($pi);
 
 // This function takes an offset ($i), and checks whether the numeric representation ($i_string) of that offset matches the offset.
-function checkPosition($pi, $i, $i_string, $i_length){
+function checkPosition($pi, $i){
+	// Typecast the position($i) to a string
+	$i_string = (string) $i;
+	// Determine the length of the string
+	$i_length = strlen($i_string);	
+	
 	for ($j = 0; $j < $i_length; $j++) { 
 		if ($pi[$i + $j] != $i_string[$j]) {
 			return false;
@@ -20,12 +25,8 @@ $start = microtime(true);
 
 // Loop through all integers up to the number of digits contained within the file.
 for ($i = 1; $i < $pi_length; $i++) { 
-	// Typecast the position($i) to a string
-	$i_string = (string) $i; 	
-	// Determine the length of the string
-	$i_length = strlen($i_string);	
-	// Feed the function the appropriate data and let it perform the check
-	if (checkPosition($pi, $i, $i_string, $i_length)){
+	// Verify for each integer whether it is a self-locating string
+	if (checkPosition($pi, $i)){
 		// If there is a match, present the self locating string
 		echo $i.'<br>';
 	}
